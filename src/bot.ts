d4620c44fbabc188682e7087e12210748207fa64
papi_tokei <hiroya.jackson@gmail.com>
2023-04-27 14:30:43 -0700

    update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,6 +8,13 @@ const token = process.env.DISCORD_BOT_TOKEN;
 console.log("Bot is starting...");
 
 const client = new Client({
-    intents: []
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
 });
+
+
 client.login(token);
